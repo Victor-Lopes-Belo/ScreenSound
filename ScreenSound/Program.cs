1a9c70a6f5aa3f2720ba2e89f55bd0b6bc7d50b9
Victor Lopes <99993119+Victor-Lopes-Belo@users.noreply.github.com>
2023-12-09 15:46:12 -0300

    Add files via upload
    inicio

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,37 @@
+﻿string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu() {
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite 0 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolida = Console.ReadLine()!;
+    int opcaoEscolitaNumerica = int.Parse(opcaoEscolida);
+
+    switch (opcaoEscolitaNumerica)
+    {
+        case 1: Console.WriteLine("Você digitou a opção " + opcaoEscolida); break;
+        case 2: Console.WriteLine("Você digitou a opção " + opcaoEscolida); break;
+        case 3: Console.WriteLine("Você digitou a opção " + opcaoEscolida); break;
+        case 4: Console.WriteLine("Você digitou a opção " + opcaoEscolida); break; 
+        case 5: Console.WriteLine("Você digitou a opção " + opcaoEscolida); break;
+        default: Console.WriteLine("opção inválida"); break;
+    }
+}
